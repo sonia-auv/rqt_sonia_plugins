fd6d71330dd2a0c419e8b91c5a11967f371a948d
Thibaut Mattio <thibaut.mattio@gmail.com>
2016-07-24 18:28:54 -0700

    Adding the callback for the proc_tree list, tested and working

--- src/rqt_mapping_client/ros_comm/services.py
+++ src/rqt_mapping_client/ros_comm/services.py
@@ -1,16 +1,15 @@
 import rospy
-from sonia_msgs.msg import *
+from sonia_msgs.srv import GetProcTreeList
 
 
 def get_proc_tree_list():
-    pass
-    # rospy.wait_for_service('/proc_mapping/get_proc_tree_list')
-    # try:
-    #     service_handler = rospy.ServiceProxy('/proc_mapping/get_proc_tree_list', GetProcTreeList)
-    #     result = service_handler()
-    #     return result
-    # except rospy.ServiceException, e:
-    #     print("Service call failed: %s" % e)
+    rospy.wait_for_service('/proc_mapping/get_proc_tree_list')
+    try:
+        service_handler = rospy.ServiceProxy('/proc_mapping/get_proc_tree_list', GetProcTreeList)
+        result = service_handler()
+        return result
+    except rospy.ServiceException, e:
+        print("Service call failed: %s" % e)
 
 
 def get_proc_unit_list(proc_tree):
