54233cf9b2b05f6efb49c65402bdb1d0c7ce7593
stephanejoncas <stephanejoncas17@hotmail.com>
2017-02-25 00:10:18 -0500

    Adding kill switch and mission switch indicator

---
+++ rqt_toolbar/src/rqt_toolbar/ToolbarKillmissionWidget.py
@@ -0,0 +1,53 @@
+import os
+import rospy
+import rospkg
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding.QtCore import pyqtSignal
+
+
+from proc_control.srv import EnableControl
+from sonia_msgs.msg import MissionSwitchMsg,PowerSupplyMsg
+
+
+class KillMissionWidget(QWidget):
+    mission_received = pyqtSignal('bool')
+    kill_received = pyqtSignal('bool')
+
+    def __init__(self):
+        super(KillMissionWidget, self).__init__()
+        # Give QObjects reasonable names
+        self.setObjectName('KillMissionWidget')
+
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_toolbar'), 'resource', 'KillMission.ui')
+        loadUi(ui_file, self)
+
+        self._mission_switch = rospy.Subscriber('/tobechange', MissionSwitchMsg, self._mission_switch_callback)
+        self.mission_received.connect(self._handle_mission_result)
+
+        self.kill_switch = rospy.Subscriber('/tobechange2',PowerSupplyMsg, self._kill_switch_callback)
+        self.kill_received.connect(self._handle_kill_result)
+
+    def _mission_switch_callback(self, data):
+        self.mission_received.emit(data.state)
+
+    def _kill_switch_callback(self,data):
+        self.kill_received.emit(data.kill_switch_state)
+
+    def _handle_mission_result(self,mission):
+        if mission:
+            print 'in true'
+            self.MissionSwitch_label.setPalette(self.paletteChecked.palette())
+        else:
+            self.MissionSwitch_label.setPalette(self.paletteUnchecked.palette())
+
+    def _handle_kill_result(self,kill_switch_state):
+        if kill_switch_state:
+            self.KillSwitch_label.setPalette(self.paletteChecked.palette())
+        else:
+            self.KillSwitch_label.setPalette(self.paletteUnchecked.palette())
+
+
+
+
