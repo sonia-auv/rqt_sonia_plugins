68d70889068685ad3cffc6c0c12e71e7fa084efe
stephanejoncas <stephanejoncas17@hotmail.com>
2017-03-06 23:53:45 -0500

    Modif rqt toolbar

--- rqt_toolbar/src/rqt_toolbar/ToolbarKillmissionWidget.py
+++ rqt_toolbar/src/rqt_toolbar/ToolbarKillmissionWidget.py
@@ -6,7 +6,7 @@ from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtCore import pyqtSignal
 
-from sonia_msgs.msg import MissionSwitchMsg, PowerSupplyMsg
+from provider_kill_mission.msg import MissionSwitchMsg,KillSwitchMsg
 
 
 class KillMissionWidget(QWidget):
@@ -21,17 +21,17 @@ class KillMissionWidget(QWidget):
         ui_file = os.path.join(rospkg.RosPack().get_path('rqt_toolbar'), 'resource', 'KillMission.ui')
         loadUi(ui_file, self)
 
-        self._mission_switch = rospy.Subscriber('/tobechange', MissionSwitchMsg, self._mission_switch_callback)
+        self._mission_switch = rospy.Subscriber('/provider_kill_mission/mission_switch_msg', MissionSwitchMsg, self._mission_switch_callback)
         self.mission_received.connect(self._handle_mission_result)
 
-        self.kill_switch = rospy.Subscriber('/tobechange2',PowerSupplyMsg, self._kill_switch_callback)
+        self.kill_switch = rospy.Subscriber('/provider_kill_mission/kill_switch_msg',KillSwitchMsg, self._kill_switch_callback)
         self.kill_received.connect(self._handle_kill_result)
 
     def _mission_switch_callback(self, data):
         self.mission_received.emit(data.state)
 
     def _kill_switch_callback(self, data):
-        self.kill_received.emit(data.kill_switch_state)
+        self.kill_received.emit(data.state)
 
     def _handle_mission_result(self,mission):
         if mission:
