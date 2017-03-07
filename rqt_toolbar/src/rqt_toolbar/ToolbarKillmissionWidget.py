ce40fb8f1014595306d30949c698344310cce24d
kecoo <kevin.coombs.cem@gmail.com>
2017-03-07 13:49:05 -0500

    Add call to service on telemetry start to sync with provider_kill_mission

--- rqt_toolbar/src/rqt_toolbar/ToolbarKillmissionWidget.py
+++ rqt_toolbar/src/rqt_toolbar/ToolbarKillmissionWidget.py
@@ -3,11 +3,11 @@ import rospy
 import rospkg
 
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QAction, QMenu, QWidget,QActionGroup
+from python_qt_binding.QtWidgets import QAction, QMenu, QWidget, QActionGroup
 from python_qt_binding.QtCore import pyqtSignal, Qt
-
 from provider_kill_mission.msg import MissionSwitchMsg, KillSwitchMsg
-from provider_kill_mission.srv import OverrideMissionSwitch,OverrideMissionSwitchRequest, SetMissionSwitch, SetMissionSwitchRequest
+from provider_kill_mission.srv import OverrideMissionSwitch, OverrideMissionSwitchRequest, SetMissionSwitch, \
+    SetMissionSwitchRequest, GetOverrideMissionSwitch, GetMissionSwitch, GetKillSwitch
 
 
 class KillMissionWidget(QWidget):
@@ -35,6 +35,35 @@ class KillMissionWidget(QWidget):
 
         self._create_contextual_menu()
 
+        self.initialize_components()
+
+    def initialize_components(self):
+        try:
+            # Init override status
+            get_override_mission_switch_state = rospy.ServiceProxy(
+                '/provider_kill_mission/get_override_mission_switch_state',
+                GetOverrideMissionSwitch)
+            override_state = get_override_mission_switch_state()
+            self._override_action.setChecked(bool(override_state.state))
+            self._mission_switch_on_action.setEnabled(bool(override_state.state))
+            self._mission_switch_off_action.setEnabled(bool(override_state.state))
+            get_mission_switch_state = rospy.ServiceProxy('provider_kill_mission/get_mission_switch_state',
+                                                          GetMissionSwitch)
+            #Init missions status
+            mission_state = get_mission_switch_state()
+            self._mission_switch_on_action.setChecked(bool(mission_state.state))
+            self._mission_switch_off_action.setChecked(not bool(mission_state.state))
+            self._handle_mission_result(bool(mission_state.state))
+
+            #Init kill status
+            get_kill_switch_state = rospy.ServiceProxy('provider_kill_mission/get_kill_switch_state',
+                                                          GetKillSwitch)
+            kill_state = get_kill_switch_state()
+            self._handle_kill_result(bool(kill_state.state))
+        except rospy.ServiceException, e:
+            print e
+            rospy.logerr('Mission Executor is not starteddd')
+
     def _create_contextual_menu(self):
         self._menu = QMenu(self.mission_switch_widget)
 
@@ -59,20 +88,19 @@ class KillMissionWidget(QWidget):
         self._mission_switch_off_action.setEnabled(False)
         self._mission_switch_off_action.setChecked(True)
 
-    def _override_mission_switch(self,checked):
+    def _override_mission_switch(self, checked):
         try:
-            override_mission_srv = rospy.ServiceProxy('/provider_kill_mission/override_mission_switch', OverrideMissionSwitch)
+            override_mission_srv = rospy.ServiceProxy('/provider_kill_mission/override_mission_switch',
+                                                      OverrideMissionSwitch)
             override_mission_req = OverrideMissionSwitchRequest()
             override_mission_req.state = int(checked)
             override_mission_srv(override_mission_req)
-            print 'hey'
 
             self._mission_switch_on_action.setEnabled(checked)
             self._mission_switch_off_action.setEnabled(checked)
 
         except rospy.ServiceException, e:
             rospy.logerr('provider Kill mission is not started')
-
 
     def _override_mission_switch_on(self):
         try:
