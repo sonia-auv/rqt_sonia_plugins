ec0312c39df54e1fa3afb25ce43b3d392835f95f
olavoie <olavoie9507@gmail.com>
2017-05-12 10:32:59 -0400

    ui for simulate buoy position

---
+++ rqt_sim_vision/src/rqt_sim_vision/SimVisionWidget.py
@@ -0,0 +1,134 @@
+import os
+import rospkg
+import rospy
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtWidgets import QMainWindow, QWidget
+from python_qt_binding.QtGui import QPainter, QColor, QPen, QBrush
+from python_qt_binding.QtCore import pyqtSignal
+from proc_image_processing.msg import VisionTarget
+from nav_msgs.msg import Odometry
+
+
+class SimVisionWidget(QMainWindow):
+    odom_result_data = pyqtSignal(Odometry)
+    width = 100
+    height = 100
+
+    buoy_diameter = 0.23
+
+    pixel_to_meter = (width / buoy_diameter + height / buoy_diameter) / 2
+
+    def __init__(self):
+        super(SimVisionWidget, self).__init__()
+        # Give QObjects reasonable names
+        self.setObjectName('SimVisionWidget')
+
+        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_sim_vision'), 'resource', 'mainwindow.ui')
+
+        loadUi(ui_file, self)
+
+        self.odom_result_data.connect(self.show_buoys_position)
+
+        self.mousePressEvent = self.my_mouse_press_event
+
+        self.displayed = 0
+
+        self.position_x = 0
+        self.position_y = 0
+
+        self.data_y = 0
+        self.data_z = 0
+
+        self.painter = QPainter()
+
+        self.posx_sub = -20
+        self.posy_sub = -20
+
+        self.scale_value = 0
+
+        self._odom_subscriber = rospy.Subscriber('/proc_navigation/odom', Odometry, self._odom_callback)
+
+        self._publish_image_data = rospy.Publisher('/proc_image_processing/data', VisionTarget, queue_size=10)
+
+        self.setObjectName('MySimVisionWidget')
+
+    def _odom_callback(self, data):
+        self.odom_result_data.emit(data)
+
+    def my_mouse_press_event(self, mouseEvent):
+        self.displayed = 1
+        self.position_x = mouseEvent.x() - 10
+        self.position_y = mouseEvent.y() - 10
+        print mouseEvent.x() - 10
+        print mouseEvent.y() - 10
+
+    def paintEvent(self, event):
+        self.painter.begin(self)
+        self.painter.setRenderHint(QPainter.Antialiasing)
+        self.painter.setPen(QColor(0, 0, 0))
+
+        sub_x = self.posx_sub
+        sub_y = self.posy_sub
+
+        self.painter.setBrush(QBrush(QColor(100, 100, 255)))
+        self.painter.drawEllipse(sub_x, sub_y, self.width, self.height)
+
+        self.painter.end()
+
+    def set_data(self, pos_x, pos_y):
+        data = VisionTarget()
+
+        data.header = 'simulation'
+        data.x = pos_y
+        data.y = pos_x
+        data.width = self.width
+        data.height = self.height
+        data.angle = 0.0
+        data.desc_1 = 'simulation'
+        data.desc_2 = 'simulation'
+
+        return data
+
+    def show_buoys_position(self, posData):
+        if self.displayed == 1:
+            self.data_y = int(posData.pose.pose.position.y)
+            self.data_z = int(posData.pose.pose.position.z)
+            self.test()
+
+    def test(self):
+        posy = self.data_y
+        posz = self.data_z
+        mouse_x = self.position_x
+        mouse_y = self.position_y
+        posx_bouy_sub = 300 + (mouse_x - (posy * self.pixel_to_meter + 300))
+        posy_bouy_sub = 250 + (mouse_y - (posz * self.pixel_to_meter + 250))
+
+        data = self.set_data(posx_bouy_sub, posy_bouy_sub)
+        self.posx_sub = posx_bouy_sub
+        self.posy_sub = posy_bouy_sub
+
+        self._publish_image_data.publish(data)
+
+        self.update()
+
+    def _handle_start_test_triggered(self):
+        pass
+
+    def _execute_test(self):
+        pass
+
+    def shutdown_plugin(self):
+        self._publish_image_data.unregister()
+        self._odom_subscriber.unregister()
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
---
+++ rqt_sim_vision/src/rqt_sim_vision/sim_vision_control.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from qt_gui.plugin import Plugin
+from SimVisionWidget import SimVisionWidget
+
+
+class SimulateVisionControl(Plugin):
+
+    def __init__(self, context):
+        super(SimulateVisionControl, self).__init__(context)
+        self.setObjectName('SimulateVision')
+        from argparse import ArgumentParser
+        parser = ArgumentParser()
+        parser.add_argument("-q", "--quiet", action="store_true",
+                      dest="quiet",
+                      help="Put plugin in silent mode")
+        args, unknowns = parser.parse_known_args(context.argv())
+        if not args.quiet:
+            print 'arguments: ', args
+            print 'unknowns: ', unknowns
+
+        # Create QWidget
+        self._mainWindow = SimVisionWidget()
+        # Get path to UI file which should be in the "resource" folder of this package
+
+        self._mainWindow.setWindowTitle(self._mainWindow.windowTitle())
+        if context.serial_number() > 1:
+            self._mainWindow.setWindowTitle(self._mainWindow.windowTitle() + (' (%d)' % context.serial_number()))
+        self._mainWindow.setPalette(context._handler._main_window.palette())
+        self._mainWindow.setAutoFillBackground(True)
+        # Add widget to the user interface
+        context.add_widget(self._mainWindow)
+
+    def shutdown_plugin(self):
+        self._mainWindow.shutdown_plugin()
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
