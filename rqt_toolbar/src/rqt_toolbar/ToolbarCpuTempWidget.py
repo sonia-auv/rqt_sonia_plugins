d02df77f1c527a5e75795a69d737a0020822b8fd
Marc-Antoine Couture <coumarc9@outlook.com>
2017-06-10 16:46:03 -0400

    CPU temperature is now shown in toolbar

--- rqt_toolbar/src/rqt_toolbar/ToolbarCpuTempWidget.py
+++ rqt_toolbar/src/rqt_toolbar/ToolbarCpuTempWidget.py
@@ -1,9 +1,12 @@
 import os
+import rospy
 import rospkg
 
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtCore import pyqtSignal
+
+from provider_system.msg import SystemTemperature
 
 class CpuTempWidget(QWidget):
 
@@ -14,5 +17,10 @@ class CpuTempWidget(QWidget):
         ui_file = os.path.join(rospkg.RosPack().get_path('rqt_toolbar'), 'resource', 'temp_sensor.ui')
         loadUi(ui_file, self)
 
+        rospy.Subscriber('/provider_system/system_temperature', SystemTemperature, self.system_temperature_callback)
 
-        self.temp_value.setText("{0} C".format(25))
+        self.temp_value.setText("{0} C".format("?"))
+
+
+    def system_temperature_callback(self, data):
+        self.temp_value.setText("{0} C".format(data.cpuTemp))
