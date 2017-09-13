d3c02bedd89a5c3fe3476f8eb4db9e9492a5089c
Martin Gauthier <gauthiermartin86@gmail.com>
2017-09-13 14:33:36 -0400

    WIP working on hydro ping widget

--- rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
+++ rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
@@ -6,39 +6,37 @@ from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtCore import pyqtSignal
 
-from provider_hydrophone.msg import PingMsg as provider_hydro_ping_msg
-from proc_hydrophone.msg import PingPose as proc_hydro_ping_msg
+from provider_hydrophone.msg import PingMsg
 
 
 class HydroPingWidget(QWidget):
-    monitor_hydro_ping_msg = pyqtSignal('PyQt_PyObject')
+    _monitor_hydro_ping_msg = pyqtSignal('PyQt_PyObject')
 
     def __init__(self):
         super(HydroPingWidget, self).__init__()
-
         ui_file = os.path.join(rospkg.RosPack().get_path('rqt_hydro_ping'), 'resource', 'mainwindow.ui')
         loadUi(ui_file, self)
+
         self.setWindowTitle('Hydro Ping')
 
-        self.provider_hydro_subscriber = rospy.Subscriber('/provider_hydrophone/provider_hydrophone', provider_hydro_ping_msg,)
+        self._provider_hydro_subscriber = rospy.Subscriber('/provider_hydrophone/provider_hydrophone', PingMsg,
+                                                          self._handle_provider_hydro_ping_msg)
 
-        self._thruster_subscriber = rospy.Subscriber('/provider_thruster/thruster_effort', ThrusterEffort,
-                                                     self._handle_thruster_msg)
+        self.slider_hydro_freq.valueChanged.connect(self._handle_hydro_frequency_slider_change)
 
-        self.monitor_hydro_ping_msg.connect(self._received_thruster_msg)
+        self._monitor_hydro_ping_msg.connect(self._received_provider_hydro_ping_msg)
 
-    def _handle_thruster_msg(self, msg):
-        pass
-        pass
 
-    def _received_thruster_msg(self, msg):
-        pass
+    def _handle_provider_hydro_ping_msg(self, msg):
+        self._monitor_hydro_ping_msg.emit(msg)
 
-    def _set_thruster_value(self, thruster_name, value):
-        #eval('self.' + thruster_name + 'value').setText('{}'.format(int(value)) + ' %')
-        #eval('self.' + thruster_name + 'slider').setValue(int(value))
+
+    def _handle_hydro_frequency_slider_change(self):
+        freq_value = self.slider_hydro_freq.value()
+        self.lbl_hydro_freq_value.setText('{} Hz'.format(freq_value))
+
+    def _received_provider_hydro_ping_msg(self, msg):
         pass
 
     def shutdown_plugin(self):
-        #self._thruster_subscriber.unregister()
-        pass
+        self._provider_hydro_subscriber.unregister()
