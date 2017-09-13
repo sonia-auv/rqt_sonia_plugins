411a9115058317d35c0980f4156c33c77b53d31c
Martin Gauthier <gauthiermartin86@gmail.com>
2017-09-13 14:46:45 -0400

    Completed hydro ping receive msg handler

--- rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
+++ rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
@@ -30,13 +30,19 @@ class HydroPingWidget(QWidget):
     def _handle_provider_hydro_ping_msg(self, msg):
         self._monitor_hydro_ping_msg.emit(msg)
 
-
     def _handle_hydro_frequency_slider_change(self):
         freq_value = self.slider_hydro_freq.value()
         self.lbl_hydro_freq_value.setText('{} Hz'.format(freq_value))
 
     def _received_provider_hydro_ping_msg(self, msg):
-        pass
+        freq_value = self.slider_hydro_freq.value()
+
+        freq_value_min = freq_value - 1
+        freq_value_max = freq_value + 1
+
+        if freq_value_min >= freq_value <= freq_value_max:
+            self.table_hydro_ping_data.addItem(msg)
+
 
     def shutdown_plugin(self):
         self._provider_hydro_subscriber.unregister()
