b5f857377fa6d17b2e69060d4311f7e5c9c42e19
Martin Gauthier <gauthiermartin86@gmail.com>
2017-09-19 12:27:06 -0400

    complete hydro ping widget

--- rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
+++ rqt_hydro_ping/src/rqt_hydro_ping/HydroPingWidget.py
@@ -14,22 +14,26 @@ from provider_hydrophone.msg import PingMsg
 
 
 class HydroPingWidget(QWidget):
+    PING_LIST_MAX_ITEM_COUNT = 50
     _monitor_hydro_ping_msg = pyqtSignal('PyQt_PyObject')
 
     def __init__(self):
         super(HydroPingWidget, self).__init__()
-        ui_file = os.path.join(rospkg.RosPack().get_path('rqt_hydro_ping'), 'resource', 'mainwindow.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path(
+            'rqt_hydro_ping'), 'resource', 'mainwindow.ui')
+
         loadUi(ui_file, self)
 
-        self.setWindowTitle('Hydro Ping')
+        self.ping_list = []
 
         self._provider_hydro_subscriber = rospy.Subscriber('/provider_hydrophone/provider_hydrophone', PingMsg,
-                                                          self._handle_provider_hydro_ping_msg)
+                                                           self._handle_provider_hydro_ping_msg)
 
-        self.slider_hydro_freq.valueChanged.connect(self._handle_hydro_frequency_slider_change)
+        self.slider_hydro_freq.valueChanged.connect(
+            self._handle_hydro_frequency_slider_change)
 
-        self._monitor_hydro_ping_msg.connect(self._received_provider_hydro_ping_msg)
-
+        self._monitor_hydro_ping_msg.connect(
+            self._received_provider_hydro_ping_msg)
 
     def _handle_provider_hydro_ping_msg(self, msg):
         self._monitor_hydro_ping_msg.emit(msg)
@@ -39,38 +43,60 @@ class HydroPingWidget(QWidget):
         self.lbl_hydro_freq_value.setText('{} Hz'.format(freq_value))
 
     def _received_provider_hydro_ping_msg(self, msg):
+        self.save_ping_data(msg)
+
+    def save_ping_data(self, provider_hydro_ping_data):
+        if len(self.ping_list) == self.PING_LIST_MAX_ITEM_COUNT:
+            self.ping_list.pop(0)
+
+        self.ping_list.append(provider_hydro_ping_data)
+
+        self.handle_table_content()
+
+    def handle_table_content(self):
+
         freq_value = self.slider_hydro_freq.value()
 
         freq_range = range(freq_value - 1, freq_value + 1)
 
-        rowPosition = self.table_hydro_ping_data.rowCount()
-        self.table_hydro_ping_data.insertRow(rowPosition)
+        self.table_hydro_ping_data.setRowCount(0)
 
-        time = "{}:{}".format(msg.header.stamp.secs, msg.header.stamp.nsecs)
+        for ping_item in self.ping_list:
+            if ping_item.frequency in freq_range:
+                self.insert_ping_data_row(ping_item)
+
+    def insert_ping_data_row(self, provider_hydro_ping_data):
+
+        time = "{}:{}".format(
+            provider_hydro_ping_data.header.stamp.secs,
+            provider_hydro_ping_data.header.stamp.nsecs)
 
         time_item = QTableWidgetItem(time)
-        sequence_item = QTableWidgetItem(str(msg.header.seq))
-        noise_item = QTableWidgetItem(str(msg.noise))
-        heading_item = QTableWidgetItem(str(msg.heading))
-        amplitude_item = QTableWidgetItem(str(msg.amplitude))
-        elevation_item = QTableWidgetItem(str(msg.elevation))
+        sequence_item = QTableWidgetItem(
+            str(provider_hydro_ping_data.header.seq))
+        frequency_item = QTableWidgetItem(
+            str(provider_hydro_ping_data.frequency))
+        noise_item = QTableWidgetItem(str(provider_hydro_ping_data.noise))
+        heading_item = QTableWidgetItem(str(provider_hydro_ping_data.heading))
+        amplitude_item = QTableWidgetItem(
+            str(provider_hydro_ping_data.amplitude))
+        elevation_item = QTableWidgetItem(
+            str(provider_hydro_ping_data.elevation))
 
-        self.table_hydro_ping_data.setItem(rowPosition, 0, time_item)
-        self.table_hydro_ping_data.setItem(rowPosition, 1, sequence_item)
-        self.table_hydro_ping_data.setItem(rowPosition, 2, noise_item)
-        self.table_hydro_ping_data.setItem(rowPosition, 3, heading_item)
-        self.table_hydro_ping_data.setItem(rowPosition, 4, amplitude_item)
-        self.table_hydro_ping_data.setItem(rowPosition, 5, elevation_item)
+        row_position = self.table_hydro_ping_data.rowCount()
+
+        self.table_hydro_ping_data.setRowHeight(row_position - 1, 33)
+        self.table_hydro_ping_data.insertRow(row_position)
+
+        self.table_hydro_ping_data.setItem(row_position, 0, time_item)
+        self.table_hydro_ping_data.setItem(row_position, 1, sequence_item)
+        self.table_hydro_ping_data.setItem(row_position, 2, frequency_item)
+        self.table_hydro_ping_data.setItem(row_position, 3, noise_item)
+        self.table_hydro_ping_data.setItem(row_position, 4, heading_item)
+        self.table_hydro_ping_data.setItem(row_position, 5, amplitude_item)
+        self.table_hydro_ping_data.setItem(row_position, 6, elevation_item)
 
         self.table_hydro_ping_data.resizeRowsToContents()
-
-        print(freq_range)
-        #if freq_value in freq_range:
-         #   time = "{}:{}".format(msg.header.stamp.secs, msg.header.stamp.nsecs)
-         #   ls = [time, msg.header.seq, msg.noise, msg.heading,  msg.amplitude, msg.elevation]
-            #print(ls)
-        #self.table_hydro_ping_data.addItems(ls)
-
 
     def shutdown_plugin(self):
         self._provider_hydro_subscriber.unregister()
