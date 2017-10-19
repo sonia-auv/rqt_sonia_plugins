baad5f8770abbcea9fe5c5e860552c029e03aadd
olavoie <olavoie9507@gmail.com>
2017-10-19 15:09:48 -0400

    adding pitch and roll on toolbar

--- rqt_toolbar/src/rqt_toolbar/ToolbarEnableAxisWidget.py
+++ rqt_toolbar/src/rqt_toolbar/ToolbarEnableAxisWidget.py
@@ -15,11 +15,10 @@ class EnableAxisWidget(QWidget):
         super(EnableAxisWidget, self).__init__()
         # Give QObjects reasonable names
         try:
-            rospy.wait_for_service('/proc_control/enable_control',timeout=2)
+            rospy.wait_for_service('/proc_control/enable_control', timeout=2)
         except rospy.ROSException:
             False
             # do nothing
-
 
         self.setObjectName('EnableAxisWidget')
 
@@ -34,6 +33,8 @@ class EnableAxisWidget(QWidget):
         self.allAxis.clicked[bool].connect(self._handle_allAxis_clicked)
         self.xyAxis.clicked[bool].connect(self._handle_xyAxis_clicked)
         self.depthAxis.clicked[bool].connect(self._handle_depthAxis_clicked)
+        self.rollAxis.clicked[bool].connect(self._handle_rollAxis_clicked)
+        self.pitchAxis.clicked[bool].connect(self._handle_pitchAxis_clicked)
         self.yawAxis.clicked[bool].connect(self._handle_yawAxis_clicked)
 
         self.allAxis.click()
@@ -45,18 +46,26 @@ class EnableAxisWidget(QWidget):
             self.xyAxis.toggle()
         if self.depthAxis.isChecked() != checked:
             self.depthAxis.toggle()
+        if self.rollAxis.isChecked() != checked:
+            self.rollAxis.toggle()
+        if self.pitchAxis.isChecked() != checked:
+            self.pitchAxis.toggle()
         if self.yawAxis.isChecked() != checked:
             self.yawAxis.toggle()
 
         if checked:
             self.xyAxis.setPalette(self.paletteChecked.palette())
             self.depthAxis.setPalette(self.paletteChecked.palette())
+            self.rollAxis.setPalette(self.paletteChecked.palette())
+            self.pitchAxis.setPalette(self.paletteChecked.palette())
             self.yawAxis.setPalette(self.paletteChecked.palette())
             self.allAxis.setPalette(self.paletteChecked.palette())
             self._enable_axis(X=1, Y=1, Z=1, PITCH=1, ROLL=1, YAW=1)
         else:
             self.xyAxis.setPalette(self.paletteUnchecked.palette())
             self.depthAxis.setPalette(self.paletteUnchecked.palette())
+            self.pitchAxis.setPalette(self.paletteUnchecked.palette())
+            self.rollAxis.setPalette(self.paletteUnchecked.palette())
             self.yawAxis.setPalette(self.paletteUnchecked.palette())
             self.allAxis.setPalette(self.paletteUnchecked.palette())
             if not self.new_toolbar_is_load:
@@ -65,26 +74,42 @@ class EnableAxisWidget(QWidget):
     def _handle_xyAxis_clicked(self, checked):
         if checked:
             self.xyAxis.setPalette(self.paletteChecked.palette())
-            self._enable_axis(X=1, Y=1,Z=-1,PITCH=-1,ROLL=-1, YAW=-1)
-        else :
+            self._enable_axis(X=1, Y=1, Z=-1, PITCH=-1, ROLL=-1, YAW=-1)
+        else:
             self.xyAxis.setPalette(self.paletteUnchecked.palette())
-            self._enable_axis(X=0, Y=0,Z=-1,PITCH=-1,ROLL=-1, YAW=-1)
+            self._enable_axis(X=0, Y=0, Z=-1, PITCH=-1, ROLL=-1, YAW=-1)
 
     def _handle_depthAxis_clicked(self, checked):
         if checked:
             self.depthAxis.setPalette(self.paletteChecked.palette())
-            self._enable_axis(X=-1, Y=-1,Z=1,PITCH=-1,ROLL=-1, YAW=-1)
-        else :
+            self._enable_axis(X=-1, Y=-1, Z=1, PITCH=-1, ROLL=-1, YAW=-1)
+        else:
             self.depthAxis.setPalette(self.paletteUnchecked.palette())
-            self._enable_axis(X=-1, Y=-1,Z=0,PITCH=-1,ROLL=-1, YAW=-1)
+            self._enable_axis(X=-1, Y=-1, Z=0, PITCH=-1, ROLL=-1, YAW=-1)
+
+    def _handle_rollAxis_clicked(self, checked):
+        if checked:
+            self.rollAxis.setPalette(self.paletteChecked.palette())
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=-1, ROLL=1, YAW=-1)
+        else:
+            self.rollAxis.setPalette(self.paletteUnchecked.palette())
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=-1, ROLL=0, YAW=-1)
+
+    def _handle_pitchAxis_clicked(self, checked):
+        if checked:
+            self.pitchAxis.setPalette(self.paletteChecked.palette())
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=1, ROLL=-1, YAW=-1)
+        else:
+            self.pitchAxis.setPalette(self.paletteUnchecked.palette())
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=0, ROLL=-1, YAW=-1)
 
     def _handle_yawAxis_clicked(self, checked):
         if checked:
             self.yawAxis.setPalette(self.paletteChecked.palette())
-            self._enable_axis(X=-1, Y=-1,Z=-1,PITCH=-1,ROLL=-1, YAW=1)
-        else :
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=-1, ROLL=-1, YAW=1)
+        else:
             self.yawAxis.setPalette(self.paletteUnchecked.palette())
-            self._enable_axis(X=-1, Y=-1,Z=-1,PITCH=-1,ROLL=-1, YAW=0)
+            self._enable_axis(X=-1, Y=-1, Z=-1, PITCH=-1, ROLL=-1, YAW=0)
 
     def _enable_axis(self, X, Y, Z, PITCH, ROLL, YAW):
         try:
