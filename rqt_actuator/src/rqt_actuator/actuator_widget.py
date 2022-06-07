278d8332a864f69b2b7013dff1483843bb98b14e
Alexandre Desgagné <alexandre1998@live.ca>
2022-06-06 23:56:17 -0400

    Fix error for the actuator module

--- rqt_actuator/src/rqt_actuator/actuator_widget.py
+++ rqt_actuator/src/rqt_actuator/actuator_widget.py
@@ -72,5 +72,10 @@ class ActuatorWidget(QWidget):
     def restore_settings(self, plugin_settings, instance_settings):
         pass
 
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
     def shutdown_plugin(self):
         pass
