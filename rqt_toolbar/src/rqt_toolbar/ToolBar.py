32180b8aa36e1a0c9daa244fbc74a05a9592850c
Alexandre Desgagné <alexandre1998@live.ca>
2022-06-11 16:06:47 -0400

    Toolbar control mode module

--- rqt_toolbar/src/rqt_toolbar/ToolBar.py
+++ rqt_toolbar/src/rqt_toolbar/ToolBar.py
@@ -5,12 +5,12 @@ import rospkg
 from qt_gui.plugin import Plugin
 from python_qt_binding.QtWidgets import QMainWindow, QToolBar
 
-from ToolbarEnableAxisWidget import EnableAxisWidget
-from ToolbarBatteryWidget import BatteryWidget
-from ToolbarCpuTempWidget import CpuTempWidget
-from ToolbarKillmissionWidget import KillMissionWidget
-from ToolbarCameraWidget import CameraWidget
-from Palette import Palette
+from .ToolbarSetControlMode import SetModeControlWidget
+# from .ToolbarBatteryWidget import BatteryWidget
+# from .ToolbarCpuTempWidget import CpuTempWidget
+# from .ToolbarKillmissionWidget import KillMissionWidget
+# from .ToolbarCameraWidget import CameraWidget
+from .Palette import Palette
 
 
 class ToolBar(Plugin):
@@ -37,27 +37,26 @@ class ToolBar(Plugin):
 
         self._toolbar = QToolBar()
         self._palette = Palette()
-        self._enableAxisWidget = EnableAxisWidget()
-        self._killMissionWidget = KillMissionWidget()
-        self._camera = CameraWidget()
-        context._handler._main_window.setPalette(self._palette.palette())
-        self._batteryWidget1 = BatteryWidget(1, 1)
-        self._batteryWidget2 = BatteryWidget(2, 3)
-        self._tempWidget1 = CpuTempWidget('/provider_system/system_temperature', 'Babe')
+        self._setControlModeWidget = SetModeControlWidget()
+        # self._killMissionWidget = KillMissionWidget()
+        # self._camera = CameraWidget()
+        # context._handler._main_window.setPalette(self._palette.palette())
+        # self._batteryWidget1 = BatteryWidget(1, 1)
+        # self._batteryWidget2 = BatteryWidget(2, 3)
+        # self._tempWidget1 = CpuTempWidget('/provider_system/system_temperature', 'Babe')
 
         # Add widget to the user interface
-        self._toolbar.addWidget(self._enableAxisWidget)
-        self._toolbar.addWidget(self._camera)
-        self._toolbar.addWidget(self._tempWidget1)
-        self._toolbar.addWidget(self._batteryWidget1)
-        self._toolbar.addWidget(self._batteryWidget2)
-        self._toolbar.addWidget(self._killMissionWidget)
+        self._toolbar.addWidget(self._setControlModeWidget)
+        # self._toolbar.addWidget(self._camera)
+        # self._toolbar.addWidget(self._tempWidget1)
+        # self._toolbar.addWidget(self._batteryWidget1)
+        # self._toolbar.addWidget(self._batteryWidget2)
+        # self._toolbar.addWidget(self._killMissionWidget)
 
         context.add_toolbar(self._toolbar)
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
-        self._killMissionWidget.shutdown_plugin()
         pass
 
     def save_settings(self, plugin_settings, instance_settings):
@@ -67,7 +66,7 @@ class ToolBar(Plugin):
 
     def restore_settings(self, plugin_settings, instance_settings):
         # TODO restore intrinsic configuration, usually using:
-        # v = instance_settings.value(k)
+        # v = instance_settings.value(EnableAxisWidgetk)
         pass
 
     #def trigger_configuration(self):
